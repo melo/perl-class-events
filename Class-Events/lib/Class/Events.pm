e6654efe2aaddc6215ffbd1714a7278781c6db77
Pedro Melo <melo@simplicidade.org>
2008-04-24 19:33:12 +0100

    Added initial Class::Events skel created with Module::Starter
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ Class-Events/lib/Class/Events.pm
@@ -0,0 +1,107 @@
+package Class::Events;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Class::Events - The great new Class::Events!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Class::Events;
+
+    my $foo = Class::Events->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Pedro Melo, C<< <melo at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-class-events at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Class-Events>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Class::Events
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Class-Events>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Class-Events>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Class-Events>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Class-Events>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Pedro Melo, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Class::Events
